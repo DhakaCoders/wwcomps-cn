758d131ee440fa39548be681bb20092f6a84cfee
Rannojit <cserannojitsarkar22@gmail.com>
2021-02-15 15:41:05 +0600

    .

--- themes/wwcomps/assets/js/main.js
+++ themes/wwcomps/assets/js/main.js
@@ -313,6 +313,48 @@ $('.our-proj-grid').masonry({
     });
 }
 
+if( $('.latesCompititionsSlider').length ){
+    $('.latesCompititionsSlider').slick({
+      dots: true,
+      infinite: false,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      speed: 700,
+      arrows: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+
     new WOW().init();
 
 })(jQuery);
