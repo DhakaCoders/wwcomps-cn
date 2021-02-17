5ff96992b2cc583d932f69f8536087061bb4ba56
azizulsoftsw <azizul.softsw@gmail.com>
2021-02-17 20:12:15 +0600

    Update form-login.php

--- themes/wwcomps/woocommerce/myaccount/form-login.php
+++ themes/wwcomps/woocommerce/myaccount/form-login.php
@@ -51,13 +51,14 @@ do_action( 'woocommerce_before_customer_login_form' ); ?>
 					<label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-login__rememberme">
 						<input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="rememberme" value="forever" /> <span><?php esc_html_e( 'Remember me', 'woocommerce' ); ?></span>
 					</label>
+					<span class="woocommerce-LostPassword lost_password">
+						<a href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Forgot password?', 'woocommerce' ); ?></a>
+					</span>
+				</p>
+				<p class="form-row">
 					<?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
 					<button type="submit" class="woocommerce-button button woocommerce-form-login__submit" name="login" value="<?php esc_attr_e( 'Log in', 'woocommerce' ); ?>"><?php esc_html_e( 'Log in', 'woocommerce' ); ?></button>
 				</p>
-				<p class="woocommerce-LostPassword lost_password">
-					<a href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Lost your password?', 'woocommerce' ); ?></a>
-				</p>
-
 				<?php do_action( 'woocommerce_login_form_end' ); ?>
 
 			</form>
